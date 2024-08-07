62c39f4c2c1fa86c21bc497c47647695c0074a40
David Watson <14983002+watson8@users.noreply.github.com>
2024-08-07 10:44:09 +0100

    CTP-3558 coding standards headers

--- classes/event/course_module_instance_list_viewed.php
+++ classes/event/course_module_instance_list_viewed.php
@@ -1,6 +1,5 @@
 <?php
-
-// This file is part of the Certificate module for Moodle - http://moodle.org/
+// This file is part of Moodle - http://moodle.org/
 //
 // Moodle is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -14,6 +13,12 @@
 //
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @package    mod_coursework
+ * @copyright  2017 University of London Computer Centre {@link ulcc.ac.uk}
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
 
 namespace mod_coursework\event;
 
